c7f5d8d8bc9a6caf6d514b881c1c932ae7918761
Fangjun Kuang <csukuangfj@gmail.com>
2024-04-10 09:40:37 +0800

    fix windows

---
+++ build_windows_386.go
@@ -0,0 +1,7 @@
+//go:build windows && 386
+
+/*
+#cgo LDFLAGS: -L ${SRCDIR}/i686 -lsherpa-onnx-portaudio
+*/
+
+package portaudio
---
+++ build_windows_amd64.go
@@ -0,0 +1,7 @@
+//go:build windows && amd64
+
+/*
+#cgo LDFLAGS: -L ${SRCDIR}/x86_64 -lsherpa-onnx-portaudio
+*/
+
+package portaudio
