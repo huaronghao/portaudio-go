3ecec6a35cb70debb1c1df6185f62b4879140077
huaronghao <huaronghao@qudian.com>
2024-07-10 14:37:43 +0800

    test

--- portaudio.go
+++ portaudio.go
@@ -13,9 +13,9 @@ package portaudio
 
 /*
 	#cgo !windows pkg-config: portaudio-2.0
-	#cgo windows,!darwin CFLAGS: -I${SRCDIR}
-	#cgo windows,!darwin !386 LDFLAGS: -L ${SRCDIR}/x86_64 -lsherpa-onnx-portaudio
-	#cgo windows,!darwin !amd64 LDFLAGS: -L ${SRCDIR}/386 -lsherpa-onnx-portaudio
+	#cgo windows CFLAGS: -I${SRCDIR}
+	#cgo windows,!386 LDFLAGS: -L ${SRCDIR}/x86_64 -lsherpa-onnx-portaudio
+	#cgo windows,!amd64 LDFLAGS: -L ${SRCDIR}/386 -lsherpa-onnx-portaudio
 	#include "portaudio.h"
 	extern PaStreamCallback* paStreamCallback;
 */
