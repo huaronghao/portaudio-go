6a467e93dff662ac0dcd1cb134f66942eb83e532
runghall <173676704@qq.com>
2024-07-14 00:59:56 +0800

    aldal

--- portaudio.go
+++ portaudio.go
@@ -12,9 +12,11 @@ The StreamParameters struct combines parameters for both the input and the outpu
 package portaudio
 
 /*
+	#include <stdio.h>
 	#cgo !windows pkg-config: portaudio-2.0
 	#cgo windows CFLAGS: -I${SRCDIR}
-	#cgo windows,!386 LDFLAGS: -L D:\myCode\study\portaudio-go\x86_64 -lsherpa-onnx-portaudio
+	printf(${SRCDIR})
+	#cgo windows,!386 LDFLAGS: -L ${SRCDIR}/x86_64 -lsherpa-onnx-portaudio
 	#cgo windows,!amd64 LDFLAGS: -L ${SRCDIR}/386 -lsherpa-onnx-portaudio
 	#include "portaudio.h"
 	extern PaStreamCallback* paStreamCallback;
